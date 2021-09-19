ead28441d9c78d5284c7b235a8f8f7b5a52b52ad
Sonia <serrano_sonia@hotmail.com>
2021-09-19 10:31:25 -0400

    Sun m1 Pro

--- app/controllers/careers_controller.rb
+++ app/controllers/careers_controller.rb
@@ -4,4 +4,15 @@ class CareersController < ApplicationController
         render json: careers
     end
 
+    def create
+        career = Career.create!(career_params)
+        render json: career
+    end
+
+    private
+
+    def career_params
+        params.require(:career).permit(:career_name)
+    end
+
 end
